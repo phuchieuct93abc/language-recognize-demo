037cbafe7b72574b25b263f36ec2244b0d51d2c1
hieu <phuchieuct93@gmail.com>
2018-11-10 11:31:10 +0700

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const https = require('https');
+const http = require('http');
+const fs = require("fs");
+const cors = require('cors')
+
+const httpsPort = process.env.PACMAN_HTTPS_SERVER_PORT || 8080;
+const httpPort = process.env.PACMAN_HTTP_SERVER_PORT || 8443;
+
+const httpsOptions = {
+  key: fs.readFileSync("./key/test_key.pem", "utf-8"),
+  cert: fs.readFileSync("./key/test_cert.pem", "utf-8"),
+};
+
+const httpsServer = https.createServer(httpsOptions, app);
+const httpServer = http.createServer(app);
+
+app.use(cors())
+
+
+app.get('/', function (req, res) {
+  
+
+  res.json({
+      message:"hieu",
+    status: 200
+  });
+
+});
+
+
+httpsServer.listen(httpsPort, function () {
+  console.log("https server is running on " + httpsPort);
+});
+
+httpServer.listen(httpPort, function () {
+  console.log("http server is running on " + httpPort);
+});
